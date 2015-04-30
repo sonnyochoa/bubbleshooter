4eea11135a088dac02b327cca4c65eec64b26bf2
sonnyochoa <0xsonny@gmail.com>
2015-04-30 14:05:26 -0700

    Basic game skeleton

---
+++ _js/game.js
@@ -0,0 +1,13 @@
+var BubbleShoot = window.BubbleShoot || {};
+BubbleShoot.Game = (function($){
+  var Game = function(){
+    this.init = function(){
+      $(".but_start_game").bind("click",startGame);
+    };
+    var startGame = function(){
+      $(".but_start_game").unbind("click");
+      BubbleShoot.ui.hideDialog();
+    };
+  };
+  return Game;
+})(jQuery);
---
+++ _js/ui.js
@@ -0,0 +1,11 @@
+var BubbleShoot = window.BubbleShoot || {};
+BubbleShoot.ui = (function($){
+  var ui = {
+    init : function(){
+    },
+    hideDialog : function(){
+      $(".dialog").fadeOut(275);
+    }
+  };
+  return ui;
+})(jQuery);
